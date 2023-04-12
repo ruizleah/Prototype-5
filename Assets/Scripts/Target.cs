c6405313a8c91f366c3d14b911c98efefb6e78ce
ruizleah <leruiz1@s.sfusd.edu>
2023-04-12 12:04:14 -0700

    5.3 done

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -35,9 +35,13 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        gameManager.UpdateScore(pointValue);
-        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            gameManager.UpdateScore(pointValue);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
